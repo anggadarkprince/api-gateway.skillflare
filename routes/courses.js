33c761cf62721a32edd600c9fa94ab756e5e455d
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-05-12 12:56:44 +0700

    add lesson api

--- routes/courses.js
+++ routes/courses.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const courseHandler = require('./handlers/courses')
 const chapterHandler = require('./handlers/chapters')
+const lessonHandler = require('./handlers/lessons')
 
 router.post('/', courseHandler.create);
 router.get('/', courseHandler.getAll);
@@ -14,5 +15,11 @@ router.get('/:courseId/chapters', chapterHandler.getAll);
 router.get('/:courseId/chapters/:id', chapterHandler.getOne);
 router.put('/:courseId/chapters/:id', chapterHandler.update);
 router.delete('/:courseId/chapters/:id', chapterHandler.destroy);
+
+router.post('/:courseId/lessons', lessonHandler.create);
+router.get('/:courseId/lessons', lessonHandler.getAll);
+router.get('/:courseId/lessons/:id', lessonHandler.getOne);
+router.put('/:courseId/lessons/:id', lessonHandler.update);
+router.delete('/:courseId/lessons/:id', lessonHandler.destroy);
 
 module.exports = router;
